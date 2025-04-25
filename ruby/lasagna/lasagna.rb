5f189d2b785a2e8b9238fe8c5599f4bd69b19d3e
Ian Johnson <tacoda@hey.com>
2025-04-25 11:28:06 +0200

    Update ruby lasagna exercise

--- ruby/lasagna/lasagna.rb
+++ ruby/lasagna/lasagna.rb
@@ -1,12 +1,13 @@
 class Lasagna
   EXPECTED_MINUTES_IN_OVEN = 40
+  PREP_TIME_PER_LAYER = 2
 
   def remaining_minutes_in_oven(actual_minutes_in_oven)
     EXPECTED_MINUTES_IN_OVEN - actual_minutes_in_oven
   end
 
   def preparation_time_in_minutes(layers)
-    layers * 2
+    PREP_TIME_PER_LAYER * layers
   end
 
   def total_time_in_minutes(number_of_layers:, actual_minutes_in_oven:)
